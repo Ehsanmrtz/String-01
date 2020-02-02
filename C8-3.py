a30a54f718f1ff700aa5f3b233fba3a2a53d4aae
Ehsan Mortazavi <e.mortazavi82@gmail.com>
2020-02-02 18:54:26 +0330

    Added a program

---
+++ C8-3.py
@@ -0,0 +1,7 @@
+while True:
+    a=int(input("Enter first Number = "))
+    b=int(input("Enter secend Number = "))
+    c=int(input("Enter third Number = "))
+    def mo(a,b,c):
+        return (a+b+c)/3 
+    print(mo(a,b,c))
